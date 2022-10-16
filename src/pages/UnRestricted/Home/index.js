689cd3d6da0ae6388997073ca6cd2d618725a50e
Armando Pereira <armando2019ti@gmail.com>
2022-10-16 16:10:24 -0300

    feat: fix style the of screen Home

--- src/pages/UnRestricted/Home/index.js
+++ src/pages/UnRestricted/Home/index.js
@@ -7,35 +7,25 @@ function Home() {
   return auth?.user?.id ? (
     <Navigate to="/dashboard" replace={true} />
   ) : (
-    <div className="h-full md:h-screen bg-red-700 p-4 flex flex-col items-center space-y-6">
-      <header className="container flex justify-center max-w-xl p-4">
+    <div className="h-screen bg-red-700 p-4 flex flex-col items-center space-y-6">
+      <header className=" flex justify-center max-w-xl p-4">
         <img className="w-40" src="/imgs/logo-white.svg" alt="foto" />
       </header>
 
-      <div className="container max-w-5xl text-white flex-1 p-4 flex flex-col justify-center items-center space-y-6 md:flex-row md:space-y-0 md:space-x-6">
+      <div className=" max-w-5xl text-white flex-1 p-4 flex flex-col justify-center items-center space-y-6 md:flex-row md:space-y-0 md:space-x-6">
         <div className="md:flex-1">
-          <img
-            className="w-full max-w-sm flex justify-center"
-            src="/imgs/photo.png"
-            alt="foto"
-          />
+          <img className="w-full max-w-sm flex justify-center" src="/imgs/photo.png" alt="foto" />
         </div>
 
-        <div className="md:flex-1 flex flex-col space-y-6">
+        <div className="flex-1 flex flex-col space-y-6">
           <h1 className="text-xl text-center font-bold md:text-3xl md:text-left">
             Dê o seu palpite na Copa do Mundo do Catar 2022!
           </h1>
 
-          <a
-            href="/signup"
-            className="text-red-700 bg-white text-xl px-8 py-4 rounded-xl text-center"
-          >
+          <a href="/signup" className="text-red-700 bg-white text-xl px-8 py-4 rounded-xl text-center">
             Criar minha conta
           </a>
-          <a
-            href="/login"
-            className="text-white border border-white text-xl px-8 py-4 rounded-xl text-center"
-          >
+          <a href="/login" className="text-white border border-white text-xl px-8 py-4 rounded-xl text-center">
             Fazer login
           </a>
         </div>
