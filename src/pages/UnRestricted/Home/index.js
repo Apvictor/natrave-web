17d8d9368a17759d46eb3557c8210f010784983c
Armando Pereira <armando2019ti@gmail.com>
2022-10-16 15:47:06 -0300

    feat: fix style the of screen Home

--- src/pages/UnRestricted/Home/index.js
+++ src/pages/UnRestricted/Home/index.js
@@ -7,7 +7,7 @@ function Home() {
   return auth?.user?.id ? (
     <Navigate to="/dashboard" replace={true} />
   ) : (
-    <div className="h-screen bg-red-700 p-4 flex flex-col items-center space-y-6">
+    <div className="h-full md:h-screen bg-red-700 p-4 flex flex-col items-center space-y-6">
       <header className="container flex justify-center max-w-xl p-4">
         <img className="w-40" src="/imgs/logo-white.svg" alt="foto" />
       </header>
@@ -22,7 +22,7 @@ function Home() {
         </div>
 
         <div className="md:flex-1 flex flex-col space-y-6">
-          <h1 className="text-3xl text-center font-bold md:text-left">
+          <h1 className="text-xl text-center font-bold md:text-3xl md:text-left">
             Dê o seu palpite na Copa do Mundo do Catar 2022!
           </h1>
 
